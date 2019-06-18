945c9d5faea6a6fa5e351ee25bde937d753c7982
Dulcidio da Luz Sobrinho <dulcidio.sobrinho@gmail.com>
2019-06-18 01:10:43 -0300

    Alterações na classe cliente

--- graub/src/main/java/br/com/graub/modelo/Cliente.java
+++ graub/src/main/java/br/com/graub/modelo/Cliente.java
@@ -1,5 +1,37 @@
 package br.com.graub.modelo;
 
-public class Cliente {
+public class Cliente extends GenericDomain {
 
+	private String fone;
+	private String email;
+	private String rua;
+	private Pessoa pessoa;
+	
+	public String getFone() {
+		return fone;
+	}
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
+	
+	
 }
